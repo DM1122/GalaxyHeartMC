58b742e47998038fea8222ff5a48db8e629b6ed1
DM1122 <34867698+DM1122@users.noreply.github.com>
2020-06-22 15:56:24 -0400

    Fix boot scripts

--- cloud/boot/shutdown.sh
+++ cloud/boot/shutdown.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # shutdown-script-url: gs://mc-staging-bucket/cloud/boot/shutdown.sh
 
+cd ~
+
 # stop server
 sudo screen -r mcs -X stuff '/stop\n'
-
-cd ~
 
 # backup data
 sudo gsutil rsync -d -r /home/server/world gs://mc-staging-bucket/world
 sudo gsutil rsync -d -r /home/server/logs gs://mc-staging-bucket/logs
+sudo mkdir -p /home/server/crash-reports
 sudo gsutil rsync -d -r /home/server/crash-reports gs://mc-staging-bucket/crash-reports
