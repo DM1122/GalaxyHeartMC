fa10786d3567fb4974c2a97e2aa90da2b60ed86a
DM1122 <34867698+DM1122@users.noreply.github.com>
2020-06-21 23:00:56 -0400

    Fix boot scripts

--- cloud/boot/shutdown.sh
+++ cloud/boot/shutdown.sh
@@ -4,10 +4,9 @@
 # stop server
 sudo screen -r mcs -X stuff '/stop\n'
 
-sudo su
 cd ~
 
 # backup data
-gsutil rsync -d -r /home/server/world gs://mc-staging-bucket/world
-gsutil rsync -d -r /home/server/logs gs://mc-staging-bucket/logs
-gsutil rsync -d -r /home/server/crash-reports gs://mc-staging-bucket/crash-reports
+sudo gsutil rsync -d -r /home/server/world gs://mc-staging-bucket/world
+sudo gsutil rsync -d -r /home/server/logs gs://mc-staging-bucket/logs
+sudo gsutil rsync -d -r /home/server/crash-reports gs://mc-staging-bucket/crash-reports
--- cloud/boot/startup.sh
+++ cloud/boot/startup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # startup-script-url: gs://mc-staging-bucket/cloud/boot/startup.sh
 
-sudo su
 cd ~
 
-# # update instance
-# apt-get update
-# # apt-get upgrade (needs prompt?)
+# update instance
+sudo apt-get update
+# apt-get upgrade (needs prompt?)
+# sudo apt-get update && sudo apt-get -y upgrade (might not need prompt?)
 
-# # install dependencies
-# apt-get install -y default-jre-headless
-# apt-get install -y screen
+# install dependencies
+sudo apt-get install -y default-jre-headless
+sudo apt-get install -y screen
 
-# # copy server and world from bucket
-# gsutil rsync -d -r gs://mc-staging-bucket/server /home/server
-# gsutil rsync -d -r gs://mc-staging-bucket/world /home/server/world
+# copy server and world from bucket
+sudo gsutil rsync -d -r gs://mc-staging-bucket/server /home/server
+sudo gsutil rsync -d -r gs://mc-staging-bucket/world /home/server/world
 
-# # start server
-# cd /home/server
-# screen -d -m -S mcs java -Xms1G -Xmx3G -jar forge.jar nogui
+# start server
+cd /home/server
+sudo screen -d -m -S mcs java -Xms1G -Xmx3G -jar forge.jar nogui
