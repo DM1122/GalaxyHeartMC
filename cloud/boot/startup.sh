ad2da2226c402a4950e37ce6bc13a200377aa49d
DM1122 <34867698+DM1122@users.noreply.github.com>
2020-06-21 22:19:49 -0400

    Fix startup.sh

--- cloud/boot/startup.sh
+++ cloud/boot/startup.sh
@@ -2,12 +2,12 @@
 # startup-script-url: gs://mc-staging-bucket/cloud/boot/startup.sh
 
 # update instance
-apt-get update
-apt-get upgrade
+# apt-get update
+# apt-get upgrade
 
 # install dependencies
-apt-get install -y default-jre-headless
-apt-get install -y screen
+# apt-get install -y default-jre-headless
+# apt-get install -y screen
 
 # copy server and world from bucket
 gsutil rsync -d -r gs://mc-staging-bucket/server /home/server
