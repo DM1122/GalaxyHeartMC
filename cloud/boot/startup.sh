e28482314f99c1fd3da85a3713dcb8168f12caa4
DM1122 <34867698+DM1122@users.noreply.github.com>
2020-06-21 22:53:02 -0400

    fix boot scripts

--- cloud/boot/startup.sh
+++ cloud/boot/startup.sh
@@ -4,18 +4,18 @@
 sudo su
 cd ~
 
-# update instance
-apt-get update
-# apt-get upgrade (needs prompt?)
+# # update instance
+# apt-get update
+# # apt-get upgrade (needs prompt?)
 
-# install dependencies
-apt-get install -y default-jre-headless
-apt-get install -y screen
+# # install dependencies
+# apt-get install -y default-jre-headless
+# apt-get install -y screen
 
-# copy server and world from bucket
-gsutil rsync -d -r gs://mc-staging-bucket/server /home/server
-gsutil rsync -d -r gs://mc-staging-bucket/world /home/server/world
+# # copy server and world from bucket
+# gsutil rsync -d -r gs://mc-staging-bucket/server /home/server
+# gsutil rsync -d -r gs://mc-staging-bucket/world /home/server/world
 
-# start server
-cd /home/server
-screen -d -m -S mcs java -Xms1G -Xmx3G -jar forge.jar nogui
+# # start server
+# cd /home/server
+# screen -d -m -S mcs java -Xms1G -Xmx3G -jar forge.jar nogui
