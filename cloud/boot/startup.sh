73e2283ce2947e5f2dcb09310f264d0bd4a59c04
DM1122 <34867698+DM1122@users.noreply.github.com>
2020-06-21 23:03:33 -0400

    Fix boot

--- cloud/boot/startup.sh
+++ cloud/boot/startup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # startup-script-url: gs://mc-staging-bucket/cloud/boot/startup.sh
 
-cd ~
+# cd ~
 
-# update instance
-sudo apt-get update
-# apt-get upgrade (needs prompt?)
-# sudo apt-get update && sudo apt-get -y upgrade (might not need prompt?)
+# # update instance
+# sudo apt-get update
+# # apt-get upgrade (needs prompt?)
+# # sudo apt-get update && sudo apt-get -y upgrade (might not need prompt?)
 
-# install dependencies
-sudo apt-get install -y default-jre-headless
-sudo apt-get install -y screen
+# # install dependencies
+# sudo apt-get install -y default-jre-headless
+# sudo apt-get install -y screen
 
-# copy server and world from bucket
-sudo gsutil rsync -d -r gs://mc-staging-bucket/server /home/server
-sudo gsutil rsync -d -r gs://mc-staging-bucket/world /home/server/world
+# # copy server and world from bucket
+# sudo gsutil rsync -d -r gs://mc-staging-bucket/server /home/server
+# sudo gsutil rsync -d -r gs://mc-staging-bucket/world /home/server/world
 
-# start server
-cd /home/server
-sudo screen -d -m -S mcs java -Xms1G -Xmx3G -jar forge.jar nogui
+# # start server
+# cd /home/server
+# sudo screen -d -m -S mcs java -Xms1G -Xmx3G -jar forge.jar nogui
